b9157916dd18b481d3bf2c97d365d5e0b5e54ca6
LucianoTallone07 <89280377+LucianoTallone07@users.noreply.github.com>
2021-09-27 11:38:55 +0200

    Agrego funciones
    Agrego funciones para indicadores técnicos

--- Indicadores.py
+++ Indicadores.py
@@ -1,18 +1,22 @@
-def devolver_con_percentil(df):
-  data = df . copy()
-  data["variacion"]  = data["Close"].pct_change() * 100 
-  data.dropna(inplace=True)
-  data["rank_variacion"] = data["variacion"].rank()
-  data["rank_variacion_pct"] = data["variacion"].rank(pct= True)
-  return data
-
-def devolver_top_n_variacion(df, n=10, es_de_baja=True):
-  data = df . copy() 
-  data["variacion"]  = data["Close"].pct_change() * 100 
-  data.dropna(inplace=True)
-  return data.sort_values("variacion", ascending = es_de_baja ).head(n)
-
-def agregar_media_exponencial(data, periodos):
-  df = data . copy()
-  df[f"ewm_{periodos}"] = df["Close"].ewm(span= periodos)
-  return df
+def addBollinger(data, ruedas=20, desvios=2):
+    data = data.copy()
+    data['sma_20'] = data["Adj Close"].rolling(20).mean()
+    volatilidad = data["Adj Close"].rolling(20).std()
+    data['boll_inf'] = data.sma_20 - 2 * volatilidad
+    data['boll_sup'] = data.sma_20 + 2 * volatilidad
+    data.dropna(inplace=True)
+    return data
+  
+  
+  def addMACD(data, slow=26, fast=12, suavizado=9):
+    '''
+    El MACD toma la columna "Close" para hacer los calculos. Enviar datos ajustados
+    '''
+    df = data.copy()
+    df['ema_fast'] = df.Close.ewm(span=fast).mean()
+    df['ema_slow'] = df.Close.ewm(span=slow).mean()
+    data['macd'] = df.ema_fast - df.ema_slow
+    data['signal'] = data.macd.rolling(suavizado).mean()
+    data['histograma'] = data.macd - data.signal
+    data = data.dropna().round(2)
+    return data
